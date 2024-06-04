199baeaca5efefa4d6e420227387d06df8325f41
4Cafes <138708258+4Cafes@users.noreply.github.com>
2024-06-03 20:50:07 -0400

    Create index-audio.js

---
+++ webasset/script/index-audio.js
@@ -0,0 +1,27 @@
+var audio = $("#audio")[0]
+var init = false
+
+audio.volume = 1
+
+function start() {
+    if (init) return
+    else init = true
+    
+    var rythm = new Rythm()
+    rythm.addRythm('silentimg', 'image-shadow', 0, 10)
+    rythm.addRythm('title', 'text-shadow', 0, 10)
+    rythm.addRythm('text', 'text-shadow', 0, 10)
+    rythm.addRythm('motd', 'text-shadow', 0, 10)
+    rythm.connectExternalAudioElement(audio)
+    rythm.start()
+}
+
+setInterval(() => {
+    audio.play()
+    .then(() => {
+        start()
+        $(container_init).css("visibility", "hidden")
+        $(container_main).css("visibility", "visible")
+    })
+    .catch(err => {})
+}, 50)
